f331cac09aa5ccc90dc44863a3d97473d3cb9d7a
kulasisin <107702039@mail2.nccu.tw>
2023-12-07 16:58:27 +0800

    4

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
-
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 console.log("Hello world!");
-
+console.log("Hello world!");
+console.log("Hello world!");
 // added by angela
-document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenuButton = document.getElementById('mobile-menu');
-    const menu = document.querySelector('.menu');
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenuButton = document.getElementById("mobile-menu");
+  const menu = document.querySelector(".menu");
 
-    mobileMenuButton.addEventListener('click', function () {
-        menu.classList.toggle('show');
-    });
+  mobileMenuButton.addEventListener("click", function () {
+    menu.classList.toggle("show");
+  });
 });
